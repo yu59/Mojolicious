d7347e412ac6b389d0a9748be49a63e418b55cf6
yu tobaru <e145759@gmail.com>
2017-06-12 08:01:10 +0900

    output result

--- my_app/lib/MyApp/Controller/Example.pm
+++ my_app/lib/MyApp/Controller/Example.pm
@@ -17,63 +17,80 @@ sub index {
 
 	my $surface;
 	my $feature;
-#	print Dumper $surface;
 	my $info;
-	my @words; 	
 	my $sth;
+
+	# table の group_id の最大値を調べる
+	$sth = $dbh->prepare("select MAX(group_id) from result");	
+	$sth->execute;
+	my $href = $sth->fetchrow_hashref;
+	my $num = $href;
+	$num->{max}++;
+	print Dumper $num->{max};	
+
 	do {
 		# 解析結果を$infoに文字列結合していく
 		# sprintf 内で改行してもhtmlでは反映されない
 		$surface = $n->surface;
-#    	$surface = decode('utf8', $surface);
+		$surface = decode('utf8', $surface);
 		$feature = $n->feature;
-#		$feature = decode('utf8', $feature);
-#		$info .=  
-#		sprintf("%s\n",
-#			$feature,
-#			$surface
-#		);
+		$feature = decode('utf8', $feature);
+		# $info .=  
+		# sprintf("%s\n",
+		# $feature,
+		# $surface
+		# );
 
 
 		# 解析結果をログとして出力
 		printf "%s\t%s\n", $n->surface, $n->feature;
 
-	
+
 		# database に格納
-#		sth = $dbh->prepare(
-#		" 
-#			insert into result (words, result, create_timestamp)
-#			values(?, ?, now())
-#		"
-#		);
-#		$sth->execute($surface, $feature);
+		$sth = $dbh->prepare(
+		" 
+			insert into result (words, result, create_timestamp, group_id)
+			values(?, ?, now(), $num->{max})
+		"
+		);
+		$sth->execute($surface, $feature);
 	
-		# 解析結果を@wordsに格納していく
-		 push @words, $n->surface;
 
 	}while($n = $n->next);
 
-	print Dumper \@words;
+	# database から全て取ってきて@resultに格納
+	$sth = $dbh->prepare("select words, result, group_id from result");	
+	$sth->execute;
+	my @result = ();
+	while ( my $href = $sth->fetchrow_hashref) {
+		unshift @result, $href;
+	}
+	print Dumper @result;
+
+	# my $result = \@result;
+	# for (@{$result}){
+	#    printf $_->{words}; 
+	# };
 	
-	# $c->stash(words => \@words);
-	$c->render(msg => '形態素解析', analysis => $info); #, words => \@words
+	$c->stash(result => \@result);
+	$c->render(msg => '形態素解析', analysis => $info); # , result => \@result
 }
 
 sub result{
 	my $c = shift;
 	$c->render(msg => '形態素解析');
-#	my $m = Text::MeCab->new();
-#	my $str = $c->param('word');
-#	my $n = $m->parse($str);
-#	do {
-#		printf("%s\t%s\t%d\n",
-#			$n->surface,
-#			$n->feature,
-#			$n->cost
-#		);
-#	}while($n = $n->next);
-#	
-#	$c->redirect_to('/result');
+	#	my $m = Text::MeCab->new();
+	#	my $str = $c->param('word');
+	#	my $n = $m->parse($str);
+	#	do {
+		#		printf("%s\t%s\t%d\n",
+			#			$n->surface,
+			#			$n->feature,
+			#			$n->cost
+		#		);
+	#	}while($n = $n->next);
+	#	
+	#	$c->redirect_to('/result');
 } 
    
 1;
