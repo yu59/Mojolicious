49cd0e01a0b210a7ea5db21599a937e6f4016cd1
yu tobaru <e145759@gmail.com>
2017-06-12 01:58:07 +0900

    delete templates/controller/MyApp_Controller.pl

--- my_app/templates/controller/MyApp_Controller.pl
+++
@@ -1,9 +0,0 @@
-package MyApp::Controller;
-use Mojo::Base 'Mojolicious::Controller';
-use Text::MeCab;
-use Data::Dumper;
-
-sub result {
-	$c = shift;
-	$c->render(msg => 'controller/MyApp_Controller.pl');
-}
